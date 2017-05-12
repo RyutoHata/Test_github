9451ff4efc65dea6396ebae5dc4955b0ab9eeffa
Ryuto Hata <ryuto.aiit@gmail.com>
2017-05-12 13:58:59 +0900

    Add error check

--- tcp_server.c
+++ tcp_server.c
@@ -11,28 +11,52 @@ int main () {
   struct sockaddr_in client;
   int len;
   int sock;
-  
+  int n;
+    
   /*making socket*/
   sock0 = socket(AF_INET, SOCK_STREAM, 0);
+
+  /*check socket error*/
+  if ( sock0 < 0){
+    perror("socket");
+    return 1;
+  }
 
   /*setting of socket*/
   addr.sin_family = AF_INET;
   addr.sin_port = htons(12345);
   addr.sin_addr.s_addr = INADDR_ANY;
 
-  bind(sock0 , (struct sockaddr *)&addr, sizeof(addr));
+  /*bind and error check*/
+  if ( bind(sock0 , (struct sockaddr *)&addr, sizeof(addr)) != 0) {
+    perror("bind");
+    return 1;
+  }
 
   /*waiting for client*/
-  listen(sock0, 5);
+  if ( listen(sock0, 5) != 0) {
+    perror("listen");
+    return 1;
+  }
 
   while (1) {
 
     /*accepting */
     len = sizeof(client);
     sock = accept(sock0, (struct sockaddr *)&client, &len);
+    if (sock < 0 ) {
+      perror("accept");
+      return 1;
+    }     
 
     /*send HELLO*/
     write(sock, "HELLO" , 5);
+    if (n < 0 ) {
+      perror("write");
+      break;
+    }     
+
+
 
     /*session close*/
     close(sock);
